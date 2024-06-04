3c774dbbcaab4dffa66695eb8a28a16d40683251
m g <Marouane Guerouji>
2024-06-04 02:50:25 -0400

    Learned how to use npm with node where I initialized a new project at first so the a config Json file can be created. Then, I practiced integrading and using  npm packages on my project by using ESM (ECMA module) to add imports instead of using require func.

---
+++ BackEnd/2.4 QR Code Project/index.js
@@ -0,0 +1,40 @@
+/* 
+* The assingment description: 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+/**
+ * My description :
+ * Mini code to prompt the user for a  URL link and then generate a QR code image which gets saved in the current forlder.
+ * Used the following code snippets from the following links:
+ * https://www.npmjs.com/package/inquirer
+ * https://www.npmjs.com/package/qr-image
+ * Then pusrpose of this assigment is to practice using npm packages and using ESM (ECMAScript Module):
+ */
+/**
+ * Prompt the user for URL imput.
+ */
+import input from "@inquirer/input";
+/**
+ * package to generate QR image.
+ */
+import qr from "qr-image";
+/**
+ * File system package.
+ */
+import fs from "fs";
+
+/**
+ * URL entered by the user.
+ */
+const URL = await input({ message: "Enter your name" });
+
+/**
+ * Generates the QR image.
+ */
+var qr_svg = qr.image(URL, { type: "png" });
+/**
+ * Saves the QR-image to the current folder.
+ */
+qr_svg.pipe(fs.createWriteStream(URL + ".png"));
